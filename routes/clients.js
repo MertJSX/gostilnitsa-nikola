6bae7745cd01d95e8b0fcd68a52fc42abcab1363
MertJSX <mertJSX@proton.me>
2025-07-05 22:26:31 +0300

    Modified routes

--- routes/clients.js
+++ routes/clients.js
@@ -7,4 +7,25 @@ clients.get("/", (req, res) => {
     })
 })
 
+clients.get("/reservation", (req, res) => {
+    res.render(__dirname + "/../views/Reservation.ejs", {
+        layout: __dirname + "/../views/layouts/layout1.ejs",
+        title: "Резервация"
+    })
+})
+
+clients.get("/menu", (req, res) => {
+    res.render(__dirname + "/../views/Menu.ejs", {
+        layout: __dirname + "/../views/layouts/layout1.ejs",
+        title: "Меню"
+    })
+})
+
+clients.get("/contacts", (req, res) => {
+    res.render(__dirname + "/../views/Contacts.ejs", {
+        layout: __dirname + "/../views/layouts/layout1.ejs",
+        title: "Контакти"
+    })
+})
+
 module.exports = clients
--- routes/employees.js
+++ routes/employees.js
@@ -1,7 +1,59 @@
 let employees = require("express").Router();
+let yaml = require("js-yaml");
+let fs = require("fs");
+const opt = yaml.load(fs.readFileSync("settings.yml", "utf8"));
+
+employees.get("/login", (req, res) => {
+  res.render(__dirname + "/../views/admin/Login.ejs", {
+    layout: __dirname + "/../views/layouts/emptyLayout.ejs",
+    title: "Админ",
+  });
+});
+
+employees.post("/login", (req, res) => {
+  let password = req.body.password;
+
+  if (password == opt.adminPassword) {
+    req.session.username = "admin";
+    res.redirect("/admin/");
+    return;
+  }
+
+  res.redirect("/admin/login");
+});
+
+employees.use("/", (req, res, next) => {
+  if (!req.session.username) {
+    res.redirect("/admin/login");
+    return;
+  }
+  next();
+});
+
+employees.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
 
 employees.get("/", (req, res) => {
-    res.send("It's working! This route is for employees!")
-})
+  res.render(__dirname + "/../views/admin/AdminHome.ejs", {
+    layout: __dirname + "/../views/layouts/adminLayout.ejs",
+    title: "Админ",
+  });
+});
 
-module.exports = employees
+employees.get("/reservations", (req, res) => {
+  res.render(__dirname + "/../views/admin/Reservations.ejs", {
+    layout: __dirname + "/../views/layouts/adminLayout.ejs",
+    title: "Админ",
+  });
+});
+
+employees.get("/foods", (req, res) => {
+  res.render(__dirname + "/../views/admin/Foods.ejs", {
+    layout: __dirname + "/../views/layouts/adminLayout.ejs",
+    title: "Админ",
+  });
+});
+
+module.exports = employees;
