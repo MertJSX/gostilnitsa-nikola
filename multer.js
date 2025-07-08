d5107444ba2364abd77e823fe37a1985524c08f3
MertJSX <mertJSX@proton.me>
2025-07-08 23:34:00 +0300

    Added new file multer.js

---
+++ multer.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + file.originalname;
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const image = multer({ storage: storage });
+
+
+module.exports = image;
