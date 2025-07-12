eef6e9df490a06653cb5e1fcb8b2a7fdbe2396c4
MertJSX <mertJSX@proton.me>
2025-07-12 11:29:44 +0300

    Refactor websocket connection handling: remove unnecessary console logs and commented-out code for cleaner implementation

--- websocket.js
+++ websocket.js
@@ -3,17 +3,11 @@ const { WebSocketServer } = require("ws");
 const wss = new WebSocketServer({ port: 8080, noDelay: true, perMessageDeflate: false });
 
 wss.on("connection", (ws) => {
-  console.log("New client connected!");
-  ws.send("connection established");
-  ws.on("close", () => console.log("Client has disconnected!"));
+  // ws.send("connection established");
+  // ws.on("close", () => console.log("Client has disconnected!"));
   ws.on("message", (data) => {
     try {
       const parsedData = JSON.parse(data);
-      console.log(`distributing message: ${data}`);
-      console.log(`distributing message: ${parsedData}`);
-
-      console.log(parsedData);
-
       switch (parsedData.type) {
         case "food-quantity-change":
           updateFoodQuantity(parsedData.id, parsedData.quantity, (err) => {
